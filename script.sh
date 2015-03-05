e81764d4e30a84b10232a9e6a90091120ebea2dc
Ritwick Malhotra <archetype2142@users.noreply.github.com>
2015-03-05 19:29:23 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,15 @@
-#!bin/sh
-cd
+#!/bin/sh
+nano help.txt
+nano start.sh
+alias compile="g++"
 mkdir programming
 cd programming
+mkdir new
+cd new
 nano main.cpp
 g++ main.cpp
-./a.out
+mv a.out main_executable
+./main_executable
+cd
+cd CPP/PROGRAMS
+
